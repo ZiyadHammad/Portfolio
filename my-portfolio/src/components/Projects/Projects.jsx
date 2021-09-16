46268940a986166b0d515a71ae5db44db51b641f
Ziyad Hammad <ziyad.hammad@outlook.com>
2021-09-15 21:18:45 -0400

    Working on Projects

--- my-portfolio/src/components/Projects/Projects.jsx
+++ my-portfolio/src/components/Projects/Projects.jsx
@@ -5,21 +5,24 @@ export default function Projects() {
     <div className="projects" id="projects">
       <h1>Projects</h1>
       <ul>
+        <li>Ruby on Rails</li>
         <li>MERN App</li>
         <li>React App</li>
         <li>HTML, CSS & JavaScript</li>
       </ul>
-      
+
       <div className="container">
 
-      <div className="item">
-          <img src="assets/code-doc.png" alt="" />
-          <h3>Code Doctor</h3>
+        <div className="item">
+          <a href="https://znoted-app.netlify.app/">
+          <img src="assets/Noted.png" alt="" />
+          </a>
+          <h3>Noted</h3>
         </div>
 
         <div className="item">
-          <img src="assets/code-doc.png" alt="" />
-          <h3>Code Doctor</h3>
+          <img src="assets/Every.png" alt="" />
+          <h3>EveryPenny</h3>
         </div>
       
       </div>
@@ -27,13 +30,14 @@ export default function Projects() {
       <div className="container">
 
         <div className="item">
+          
           <img src="assets/code-doc.png" alt="" />
           <h3>Code Doctor</h3>
         </div>
 
         <div className="item">
-          <img src="assets/code-doc.png" alt="" />
-          <h3>Code Doctor</h3>
+          <img src="assets/Delicous.png" alt="" />
+          <h3>Delicous Recipes</h3>
         </div>
       
       </div>
